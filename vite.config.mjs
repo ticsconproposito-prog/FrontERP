d9eee80126cfebfa29e2f43ec3c934c8b6eb038f
Merlyn Mariolis Franco Arredondo <mfranco@plusti.com>
2026-05-04 23:43:21 -0600

    cambio a desarrollo.

--- vite.config.mjs
+++ vite.config.mjs
@@ -43,9 +43,9 @@ export default defineConfig(() => {
       port: 3000,
       proxy: {
         '/api': {
-          target: 'https://ferreteriaagmner.com',
+          target: 'http://127.0.0.1:8080',
           changeOrigin: true,
-          secure: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
         },
       },
     },
