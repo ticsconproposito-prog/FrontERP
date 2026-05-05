c10ef4038efd0cea5d1e84df819b66eab66283be
Merlyn Mariolis Franco Arredondo <mfranco@plusti.com>
2026-05-04 23:37:59 -0600

    se agrega cambio para control de error de CF.

--- vite.config.mjs
+++ vite.config.mjs
@@ -43,9 +43,9 @@ export default defineConfig(() => {
       port: 3000,
       proxy: {
         '/api': {
-          target: 'http://127.0.0.1:8080',
+          target: 'https://ferreteriaagmner.com',
           changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
+          secure: true,
         },
       },
     },
